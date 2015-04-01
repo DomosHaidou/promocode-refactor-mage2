ac061b778ab5851cc775cc93da9f65b0058894cb
Allan MacGregor <amacgregor@allanmacgregor.com>
2015-04-01 16:29:13 -0400

    Update ObserverSpec file

--- spec/Imagine/Promocode/Model/ObserverSpec.php
+++ spec/Imagine/Promocode/Model/ObserverSpec.php
@@ -4,16 +4,12 @@ namespace spec\Imagine\Promocode\Model;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use PhpSpec\Exception\Example\PendingException;
 
 class ObserverSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Imagine\Promocode\Model\Observer');
-    }
-
-    function it_should_validate_coupon_code(\Magento\SalesRule\Model\Coupon $coupon)
-    {
-        $coupon->getCode()->willReturn('ABC123');
     }
 }

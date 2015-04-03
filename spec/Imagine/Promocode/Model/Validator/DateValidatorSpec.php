b09f4a6737cb71f49b6e1d143092aa4db13de1bf
Allan MacGregor <amacgregor@allanmacgregor.com>
2015-04-03 15:35:51 -0400

    [WIP] Update Spec for the Date Validator

--- spec/Imagine/Promocode/Model/Validator/DateValidatorSpec.php
+++ spec/Imagine/Promocode/Model/Validator/DateValidatorSpec.php
@@ -11,4 +11,13 @@ class DateValidatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Imagine\Promocode\Model\Validator\DateValidator');
     }
+
+    function it_should_throw_an_invalid_date_exception(
+        \Magento\SalesRule\Model\Rule $rule,
+        \Magento\Sales\Model\Quote $quote
+    ) {
+        $rule->getFromDate()->willReturn('2014-01-01');
+        $exception  = new \Exception('Your coupon is no longer valid it expired on');
+        $this->shouldThrow($exception)->duringValidate($rule, $quote);
+    }
 }
